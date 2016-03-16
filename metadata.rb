37457e4f394bbc06e593df0f3b32215158bb1f9b
Sebastian Grewe <TheSerapher@users.noreply.github.com>
2016-03-16 10:46:14 +0100

    Version 0.4.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'sebastian.grewe@gmail.com'
 license           'Apache 2.0'
 description       'Installs/configures vsftpd'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.3.0'
+version           '0.4.0'
 recipe            'vsftpd::default', 'Installs/configures vsftpd'
 
 supports 'ubuntu'
