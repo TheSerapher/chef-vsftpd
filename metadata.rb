3fd833ebcc2a386bebca2c2595753356d72ca091
Sebastian Grewe <TheSerapher@users.noreply.github.com>
2016-05-04 11:51:19 +0200

    Updated metadata for Chef 12

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,8 @@
 name              'vsftpd'
 maintainer        'Sebastian Grewe'
 maintainer_email  'sebastian.grewe@gmail.com'
+issues_url        'https://github.com/TheSerapher/chef-vsftpd/issues'
+source_url        'https://github.com/TheSerapher/chef-vsftpd'
 license           'Apache 2.0'
 description       'Installs/configures vsftpd'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
