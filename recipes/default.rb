dcc1d22ee1a29f0fae9c733357bf1a996fb0c08b
Alexander Vynnyk <oleksandr.vynnyk@thomascookonline.com>
2017-01-06 11:13:52 +0200

    Fix SSL

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 
-include_recipe 'vsftpd::_install'
 include_recipe 'vsftpd::_ssl' unless node['vsftpd']['ssl']['enabled'] == false
+include_recipe 'vsftpd::_install'
 include_recipe 'vsftpd::_configure'
