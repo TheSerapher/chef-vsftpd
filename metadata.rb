52b1259eb6bd9c82addec12d8c85813ff95390a5
Alexander Vynnyk <oleksandr.vynnyk@thomascookonline.com>
2017-01-06 11:20:58 +0200

    Fix SSL

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ source_url        'https://github.com/TheSerapher/chef-vsftpd'
 license           'Apache 2.0'
 description       'Installs/configures vsftpd'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.4.0'
+version           '0.4.1'
 recipe            'vsftpd::default', 'Installs/configures vsftpd'
 
 supports 'ubuntu'
